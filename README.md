31067e9634b53ac447f2d09bbc589353870b8e49
mehfluffy <alissa.cheng@protonmail.ch>
2021-04-17 22:06:54 +0200

    Add ability to parse AMP links

--- README.md
+++ README.md
@@ -8,8 +8,11 @@ Clean URLs of tracking redirects and tags, using regular expressions. Requires o
     * economist.com/api/my-account?newsletter=1&auth=1
     * Youtube playlist tag (see also https://webapps.stackexchange.com/questions/9863/are-the-parameters-for-www-youtube-com-watch-documented)
     * duckduckgo.com/?q=mysearchstring
+* Google AMP links that have been shortened, e.g. 
+    * https://amp.abc.net.au/article/100063916 because https://www.abc.net.au/article/100063916 is not the original page, but rather https://www.abc.net.au/news/2021-04-15/australian-based-uyghurs-on-chinese-blacklist/100063916
 
 ## Ideas:
 * Domain whitelist function
 * Force https option
+* Mailing list click-tracking links usually don't have tags, just one big hash
 * Implement as chatbot or browser plugin
--- parser.py
+++ parser.py
@@ -28,25 +28,56 @@ def is_redirect(url):
 
 
 def extract_redirect(url):
-    start = re.compile('[=|(%3D)]http').search(url).span()[0] + 1
+    start = re.compile('[=|(%3D)]http').search(url).span()[0] + 1  # span gives the start,end tuple. +1 to slice from http, not =http
     return url[start:]
+
+def is_amp(url):
+    # https://medium.com/@danbuben/why-amp-is-bad-for-your-site-and-for-the-web-e4d060a4ff31
+    # Types of AMP links https://searchengineland.com/amp-links-large-281987
+    amp = re.compile('(cdn\.ampproject\.org)|(/amp)')  # /amp includes also amp. since it's usually https://amp.domain.tld/whatever
+    match = amp.search(url)
+    return match
+
+def delele_amp(url):
+    cache_amp = re.compile('cdn\.ampproject\.org/(c/)?')
+    google_amp = re.compile('google\.(\w)+/amp/(s/)?')  # Sometimes the s does not appear in google.com/amp/s/domain.tld/whatever
+    origin_amp_prepended = re.compile('amp\.')
+    
+    indices = []
+    for amp in [cache_amp, google_amp, origin_amp_prepended]:  # all are prepended tags
+        match = amp.search(url)
+        if match:
+            indices.append(match.span()[-1])
+    url = url[max(indices):]
+
+    origin_amp_appended = re.compile('/amp(\w)+/?$')  # \w ensures no symbols like dots and dash, end slash optional, $ means end of string
+    match = origin_amp_appended.search(url)
+    if match:
+        end = match.span()[0]
+    else:
+        end = None
+    return 'https://' + url[:end]
 
 
 def in_unicode(url):
     return re.compile('(%\w)+').search(url)
 
 
-def decode_url(url):
+def decode_unicode(url):
     return unquote(url)
 
 
 def parser(url):
     if is_redirect(url):
         url = extract_redirect(url)
+    if is_amp(url):
+        url = delele_amp(url)
+
     tag_indices = has_tag(url)
     if tag_indices:
         url = delete_tag(url, tag_indices)
-    if in_unicode(url):  # if decode first, saves regex 'or' but might be slower
-        url = decode_url(url)
+
+    if in_unicode(url):  # if decode first, saves the 'or' in tags & redirects but might be slower
+        url = decode_unicode(url)
     # force https?
     return url
